9e944f4ce2b3dc8739cd545a0bf11662adeb7ffc
michelleborromeo <michborromeo218@gmail.com>
2024-10-29 11:01:42 +0800

    feat: added registration functionality

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,36 +1,79 @@
 <script setup>
 import { ref } from 'vue'
-import { requiredValidator, emailValidator, passwordValidator, confirmedValidator } from '@/utils/validators'
+import {
+  requiredValidator,
+  emailValidator,
+  passwordValidator,
+  confirmedValidator,
+} from '@/utils/validators'
+import { supabase, formActionDefault } from '@/utils/supabase.js'
+import AlertNotification from '../common/AlertNotification.vue'
 
 const isPasswordVisible = ref(false)
 const isPasswordConfirmVisible = ref(false)
 const refVForm = ref()
 
 const formDataDefault = {
-firstname: '',
-lastname: '',
-email: '',
-password: '',
-roles: '',
-password_confirmation: '',
+  firstname: '',
+  lastname: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+  role: '',
 }
 
 const formData = ref({
   ...formDataDefault,
 })
 
-const onSubmit = () => {
-// alert(formData.value.email)
+const formAction = ref({
+  ...formActionDefault,
+})
+
+const onSubmit = async () => {
+  formAction.value = { ...formActionDefault }
+  formAction.value.formProcess = true
+
+  const { data, error } = await supabase.auth.signUp({
+    email: formData.value.email,
+    password: formData.value.password,
+    options: {
+      data: {
+        first_name: formData.value.firstname,
+        lastname: formData.value.lastname,
+        role: formData.value.role,
+      },
+    },
+  })
+
+  if (error) {
+    console.log(error)
+    formAction.value.formErrorMessage = error.message
+    formAction.value.formStatus = error.status
+  } else if (data) {
+    console.log(data)
+    formAction.value.formSuccessMessage = 'Successfully Registered Account.'
+    // Reset form fields after successful registration
+    refVForm.value?.reset()
+  }
+
+  formAction.value.formProcess = false
 }
 const onFormSubmit = () => {
-  refVForm.value?.validate().then(({ valid}) => {
+  refVForm.value?.validate().then(({ valid }) => {
     if (valid) onSubmit()
   })
 }
 </script>
 
 <template>
-  <v-form ref="refVForm"@submit.prevent="onFormSubmit">
+  <AlertNotification
+    :form-success-message="formAction.formSuccessMessage"
+    :form-error-message="formAction.formErrorMessage"
+  >
+  </AlertNotification>
+
+  <v-form class="mt-5" ref="refVForm" @submit.prevent="onFormSubmit">
     <v-row>
       <v-text-field
         v-model="formData.firstname"
@@ -42,7 +85,7 @@ const onFormSubmit = () => {
       ></v-text-field>
 
       <v-text-field
-      v-model="formData.lastname"
+        v-model="formData.lastname"
         label="Lastname"
         variant="outlined"
         class="v-col-6"
@@ -51,7 +94,7 @@ const onFormSubmit = () => {
     </v-row>
 
     <v-text-field
-    v-model="formData.email"
+      v-model="formData.email"
       prepend-icon="mdi-email"
       label="Email"
       variant="outlined"
@@ -60,7 +103,7 @@ const onFormSubmit = () => {
     ></v-text-field>
 
     <v-text-field
-    v-model="formData.password"
+      v-model="formData.password"
       prepend-icon="mdi-lock"
       label="Password"
       variant="outlined"
@@ -72,7 +115,7 @@ const onFormSubmit = () => {
     ></v-text-field>
 
     <v-text-field
-    v-model="formData.password_confirmation"
+      v-model="formData.password_confirmation"
       prepend-icon="mdi-lock-check"
       label="Password Confirmation"
       variant="outlined"
@@ -80,23 +123,28 @@ const onFormSubmit = () => {
       :append-inner-icon="isPasswordConfirmVisible ? 'mdi-eye-off' : 'mdi-eye'"
       @click:append-inner="isPasswordConfirmVisible = !isPasswordConfirmVisible"
       class="my-2"
-      :rules="[requiredValidator, confirmedValidator(formData.password_confirmation,formData.password)]"
+      :rules="[
+        requiredValidator,
+        confirmedValidator(formData.password_confirmation, formData.password),
+      ]"
     ></v-text-field>
 
     <v-select
-    v-model="formData.roles"
+      v-model="formData.role"
       class="pl-10"
       label="Select Role"
       :items="['Borrower', 'Librarian']"
       variant="outlined"
       density="compact"
-       :rules="[requiredValidator]"
+      :rules="[requiredValidator]"
     ></v-select>
 
     <v-btn
       class="mt-2 mx-auto d-flex"
       color="red-darken-4"
       prepend-icon="mdi-account-plus"
+      :disabled="formAction.formProcess"
+      :loading="formAction.formProcess"
       type="submit"
       >Register</v-btn
     >
