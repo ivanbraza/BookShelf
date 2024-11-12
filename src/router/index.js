e9df0a6917a4c32c13699baf35d3eed4cad751fe
Althea Loraine Buna <althealoraineee>
2024-11-12 23:43:54 +0800

    feat: added books view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterVue.vue'
+import HomePage from '@/views/system/DashboardView.vue'
+import DashboardView from '@/views/system/DashboardView.vue'
+import BooksView from '@/views/system/BooksView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +17,16 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
+    },
+    {
+      path: '/books',
+      name: 'books',
+      component: BooksView
     }
   ]
 })
--- src/views/system/DashboardView.vue
+++ src/views/system/DashboardView.vue
@@ -41,3 +41,4 @@ const { mobile } = useDisplay()
   </AppLayout>
         
 </template>
+
